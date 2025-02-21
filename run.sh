e040b4d32e79f4144e1c19dc025f9252d16c4b77
Jelte Dirks <j.dirks@outlook.com>
2025-02-21 22:04:17 +0100

    abort early when a run.sh call fails
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-glslang src/shaders/simple.vert
-glslang src/shaders/simple.frag
-cmake --build build/
-./build/Gelly
+glslang src/shaders/simple.vert && \
+  glslang src/shaders/simple.frag && \
+  cmake --build build/ && \
+  ./build/Gelly
